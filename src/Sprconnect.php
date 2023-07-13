a65f29f7ffac9fd44b88435a12a74a29c956cdc1
Jithesh KK <122150130+jitheshkk1989@users.noreply.github.com>
2023-07-13 18:17:30 +0530

    Update Sprconnect.php

--- src/Sprconnect.php
+++ src/Sprconnect.php
@@ -5,4 +5,8 @@ namespace Spr\Sprconnect;
 class Sprconnect
 {
     // Build your next great package.
+    public function yourFunction()
+    {
+        return 'Hello from your function package!';
+    }
 }
